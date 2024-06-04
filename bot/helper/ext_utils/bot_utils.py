a156bca71cb9b30deb9c5683fa031d381826486e
songokuop <149889315+songokuop@users.noreply.github.com>
2024-06-04 13:57:34 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -253,7 +253,7 @@ def get_readable_message():
         buttons.ibutton(f"{PAGE_NO}/{PAGES}", "status ref")
         buttons.ibutton("Next ▶️", "status nex")
         button = buttons.build_menu(3)
-  msg += f"<b>⛩️ Tasks</b>: {tasks}{bmax_task}"
+    msg += f"<b>⛩️ Tasks</b>: {tasks}{bmax_task}"
     msg += f"\n<b>⛩️ Bot uptime</b>: {currentTime}"
     msg += f"\n<b>⛩️ Free disk space</b>: {get_readable_file_size(disk_usage('/usr/src/app/downloads/').free)}"
     msg += f"\n<b>⛩️ Uploading speed</b>: {get_readable_file_size(up_speed)}/s"
